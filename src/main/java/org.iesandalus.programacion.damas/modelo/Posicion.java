7963b9b34a956ce7f4431b53f6aa2dad5bc1c220
ptorfer <patriciatorres83@gmail.com>
2024-10-31 00:15:54 +0100

    Creación de métodos set y get

--- src/main/java/org.iesandalus.programacion.damas/modelo/Posicion.java
+++ src/main/java/org.iesandalus.programacion.damas/modelo/Posicion.java
@@ -1,5 +1,8 @@
 package org.iesandalus.programacion.damas.modelo;
 
+import java.util.Objects;
+
+
 public class Posicion {
 
     //Atributos
@@ -12,9 +15,32 @@ public class Posicion {
         setColumna(columna);
     }
 
-    public Posicion posicion ()
+    public Posicion (Posicion posicion){
+        Objects.requireNonNull(posicion,"ERROR: No es posible copiar una posición nula.");
+        fila=posicion.fila;
+        columna=posicion.columna;
+    }
 
+    //Métodos
+    private void setFila (int fila){
+        if (fila >=1 && fila <=8){
+            this.fila=fila;
+        }
+        else{
+            throw new IllegalArgumentException("ERROR: Fila no válida.");
+        }
+    }
 
+    public int getFila() {
+        return fila;
+    }
 
-
+    private void setColumna (char columna){
+        if (columna >= 'a' && columna <= 'h'){
+            this.columna=columna;
+        }
+        else{
+            throw new IllegalArgumentException("ERROR: Columna no válida.");
+        }
+    }
 }
