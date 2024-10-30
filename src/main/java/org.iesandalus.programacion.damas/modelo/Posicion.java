820a79c3ac1f26d0e2e4a1c0f2c3b45797a13011
ptorfer <patriciatorres83@gmail.com>
2024-10-30 23:36:46 +0100

    Creación constructor Posicion

--- src/main/java/org.iesandalus.programacion.damas/modelo/Posicion.java
+++ src/main/java/org.iesandalus.programacion.damas/modelo/Posicion.java
@@ -2,7 +2,19 @@ package org.iesandalus.programacion.damas.modelo;
 
 public class Posicion {
 
+    //Atributos
     int fila;
     char columna;
+
+    //Constructores
+    public Posicion (int fila,char columna){
+        setFila(fila);
+        setColumna(columna);
+    }
+
+    public Posicion posicion ()
+
+
+
 
 }
