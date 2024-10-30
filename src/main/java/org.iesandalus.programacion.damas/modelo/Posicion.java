13d23e12d33e3b7b2e330ff86ba66b5c93b32cbc
ptorfer <patriciatorres83@gmail.com>
2024-10-31 00:19:31 +0100

    Creación método get

--- src/main/java/org.iesandalus.programacion.damas/modelo/Posicion.java
+++ src/main/java/org.iesandalus.programacion.damas/modelo/Posicion.java
@@ -43,4 +43,8 @@ public class Posicion {
             throw new IllegalArgumentException("ERROR: Columna no válida.");
         }
     }
+
+    public char getColumna (){
+        return columna;
+    }
 }
