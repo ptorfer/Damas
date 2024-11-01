fb6b47083a5a0656fe3d9b64d6d2aa5f4054fdac
ptorfer <patriciatorres83@gmail.com>
2024-11-01 21:30:54 +0100

    Creación método mostrarMenu

--- src/main/java/org.iesandalus.programacion.damas/Consola.java
+++ src/main/java/org.iesandalus.programacion.damas/Consola.java
@@ -2,4 +2,16 @@ package org.iesandalus.programacion.damas;
 
 public class Consola {
 
+    //Constructor
+    private Consola(){}
+
+    //Métodos
+    public static void mostrarMenu(){
+        System.out.println("Crear dama por defecto.");
+        System.out.println("Crear dama eligiendo el color.");
+        System.out.println("Mover.");
+        System.out.println("Salir.");
+    }
+
+
 }
