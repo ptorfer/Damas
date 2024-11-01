3f3a49abb0bdddc0a893af68723da5981803ae60
ptorfer <patriciatorres83@gmail.com>
2024-11-01 23:02:06 +0100

    creación atributo de clase

--- src/main/java/org.iesandalus.programacion.damas/MainApp.java
+++ src/main/java/org.iesandalus.programacion.damas/MainApp.java
@@ -5,9 +5,14 @@ Link github donde está el proyecto:
 https://github.com/jma982/Damas/compare/main...ptorfer:Damas:main
 */
 
+import org.iesandalus.programacion.damas.modelo.Dama;
+
 public class MainApp {
+
+    //Atributo de clase
+    private static Dama dama;
+
     public static void main(String[] args) {
-
-
     }
+
 }
